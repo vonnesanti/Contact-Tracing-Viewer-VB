212dc2bc7f6576edd8a3c2a6d9cd3245c657e3f9
Yvonne Santiago <ybsantiago@iskolarngbayan.pup.edu.ph>
2022-06-23 23:48:27 +0800

    This commit is for the clear and exit button

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -22,10 +22,119 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.exitBtn = New System.Windows.Forms.Button()
+        Me.openBtn = New System.Windows.Forms.Button()
+        Me.clearBtn = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.infoTxtbx = New System.Windows.Forms.TextBox()
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Panel1
+        '
+        Me.Panel1.BackColor = System.Drawing.Color.Transparent
+        Me.Panel1.Controls.Add(Me.Label1)
+        Me.Panel1.Controls.Add(Me.clearBtn)
+        Me.Panel1.Controls.Add(Me.openBtn)
+        Me.Panel1.Controls.Add(Me.exitBtn)
+        Me.Panel1.Controls.Add(Me.Panel2)
+        Me.Panel1.Location = New System.Drawing.Point(12, 12)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(529, 426)
+        Me.Panel1.TabIndex = 0
+        '
+        'Panel2
+        '
+        Me.Panel2.Controls.Add(Me.infoTxtbx)
+        Me.Panel2.Location = New System.Drawing.Point(17, 59)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(493, 317)
+        Me.Panel2.TabIndex = 0
+        '
+        'exitBtn
+        '
+        Me.exitBtn.BackColor = System.Drawing.Color.Khaki
+        Me.exitBtn.Font = New System.Drawing.Font("Arial Rounded MT Bold", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.exitBtn.Location = New System.Drawing.Point(17, 387)
+        Me.exitBtn.Name = "exitBtn"
+        Me.exitBtn.Size = New System.Drawing.Size(83, 33)
+        Me.exitBtn.TabIndex = 1
+        Me.exitBtn.Text = "EXIT"
+        Me.exitBtn.UseVisualStyleBackColor = False
+        '
+        'openBtn
+        '
+        Me.openBtn.BackColor = System.Drawing.Color.Khaki
+        Me.openBtn.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.openBtn.Location = New System.Drawing.Point(231, 387)
+        Me.openBtn.Name = "openBtn"
+        Me.openBtn.Size = New System.Drawing.Size(75, 33)
+        Me.openBtn.TabIndex = 2
+        Me.openBtn.Text = "OPEN"
+        Me.openBtn.UseVisualStyleBackColor = False
+        '
+        'clearBtn
+        '
+        Me.clearBtn.BackColor = System.Drawing.Color.Khaki
+        Me.clearBtn.Font = New System.Drawing.Font("Arial Rounded MT Bold", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.clearBtn.Location = New System.Drawing.Point(429, 387)
+        Me.clearBtn.Name = "clearBtn"
+        Me.clearBtn.Size = New System.Drawing.Size(81, 33)
+        Me.clearBtn.TabIndex = 3
+        Me.clearBtn.Text = "CLEAR"
+        Me.clearBtn.UseVisualStyleBackColor = False
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Khaki
+        Me.Label1.Font = New System.Drawing.Font("Mongolian Baiti", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(113, 17)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(302, 29)
+        Me.Label1.TabIndex = 4
+        Me.Label1.Text = "Contact Tracing Viewer"
+        '
+        'infoTxtbx
+        '
+        Me.infoTxtbx.Location = New System.Drawing.Point(21, 16)
+        Me.infoTxtbx.Multiline = True
+        Me.infoTxtbx.Name = "infoTxtbx"
+        Me.infoTxtbx.Size = New System.Drawing.Size(451, 286)
+        Me.infoTxtbx.TabIndex = 0
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(553, 450)
+        Me.Controls.Add(Me.Panel1)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.ShowIcon = False
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "CONTACT TRACING VIEWER"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.Panel2.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
 
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents clearBtn As Button
+    Friend WithEvents openBtn As Button
+    Friend WithEvents exitBtn As Button
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents Label1 As Label
+    Friend WithEvents infoTxtbx As TextBox
 End Class
--- Form1.vb
+++ Form1.vb
@@ -1,3 +1,22 @@
-﻿Public Class Form1
+﻿Imports System.IO
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Panel1.BackColor = Color.FromArgb(100, 0, 0, 0)
+        Panel2.BackColor = Color.FromArgb(100, 0, 0, 0)
+    End Sub
 
+    Private Sub openBtn_Click(sender As Object, e As EventArgs) Handles openBtn.Click
+
+    End Sub
+
+    Private Sub clearBtn_Click(sender As Object, e As EventArgs) Handles clearBtn.Click
+        infoTxtbx.Clear()
+    End Sub
+
+    Private Sub exitBtn_Click(sender As Object, e As EventArgs) Handles exitBtn.Click
+        Dim GameExit As DialogResult = MessageBox.Show("Are you sure you want to exit?", "CONTACT TRACING VIEWER", MessageBoxButtons.YesNoCancel, MessageBoxIcon.Information)
+        If GameExit = DialogResult.Yes Then
+            Application.Exit()
+        End If
+    End Sub
 End Class
